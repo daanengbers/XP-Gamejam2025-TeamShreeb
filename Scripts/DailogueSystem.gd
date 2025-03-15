89ccc739513a3a8115364da5e5544d5395b86627
daanengbers <daan.engbers@gmail.com>
2025-03-15 22:14:55 +0100

    Some basic stuff

---
+++ Scripts/DailogueSystem.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var DailogueArray = ["",""]
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _process(delta):
+	
+	
+	pass
+
+func PlayDailogue():
+	for i in DailogueArray:
+		
