e44b0937c51b14327722e9989f79392828fc7625
WeenterMakesGames <weentertheperson@gmail.com>
2025-03-22 09:54:37 +0100

    Some camera animations

---
+++ Scripts/impact_effect_1.gd
@@ -0,0 +1,5 @@
+extends Node3D
+
+func _ready():
+	position = Vector3(0,2,0)
+	$Anim.play("effect")
--- Scripts/room.gd
+++ Scripts/room.gd
@@ -1,5 +1,9 @@
 extends Node
 
+var IMPACT_1 = preload("res://Scenes/Effects/impact_effect_1.tscn")
+var IMPACT_2 = preload("res://Scenes/Effects/impact_effect_2.tscn")
+
+
 var roomnumber = 0
 
 var room_finished = false
@@ -11,23 +15,36 @@ func _process(delta):
 		screenZoomAttack()
 	if Input.is_action_just_pressed("test3"):
 		screenSuperAttack()
+	if Input.is_action_just_pressed("test4"):
+		spawnImpactEffect1()
 
 func screenShakeMild():
 	$ThreeD_View/ThreeD/Main_Camera/CamAnim.play("RESET")
 	$ThreeD_View/ThreeD/Main_Camera/CamAnim.play("shake")
-	pass
 
 func screenZoomAttack():
 	$ThreeD_View/ThreeD/Main_Camera/CamAnim.play("RESET")
 	$ThreeD_View/ThreeD/Main_Camera/CamAnim.play("zoom_out_and_attack")
-	pass
 
 func screenSuperAttack():
 	$ThreeD_View/ThreeD/Main_Camera/CamAnim.play("RESET")
 	$ThreeD_View/ThreeD/Main_Camera/CamAnim.play("ultra_physical_attack")
-	pass
+
+func screenShootFireball():
+	$ThreeD_View/ThreeD/Main_Camera/CamAnim.play("RESET")
+	$ThreeD_View/ThreeD/Main_Camera/CamAnim.play("fireball_throw")
 
 func goToNextScene():
 	pass
 
 
+
+# Spawn effects
+
+func spawnImpactEffect1():
+	var eff = IMPACT_1.instantiate()
+	get_parent().add_child.call_deferred(eff)
+
+func spawnImpactEffect2():
+	var eff = IMPACT_2.instantiate()
+	get_parent().add_child.call_deferred(eff)
